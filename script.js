ad3b324384e8986275e4d5451d7c350a6d2177c5
Grambot-ops <Mukizamaximus08@gmail.com>
2024-04-12 15:44:57 +0200

    Add the work need for a good start for the project

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById("buy-button").addEventListener("click", function () {});
+
+document
+  .getElementById("sell-button")
+  .addEventListener("click", function () {});
+
+document
+  .getElementById("bake-button")
+  .addEventListener("click", function () {});
+
+document
+  .getElementById("Market-button")
+  .addEventListener("click", function () {});
+document
+  .getElementById("Ingredients-button")
+  .addEventListener("click", function () {});
