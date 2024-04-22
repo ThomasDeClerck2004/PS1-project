bce7f955db0c5236ca760e67ee3a769c2fbe73e3
SebastiaanTM <r0998184@student.thomasmore.be>
2024-04-22 15:05:04 +0200

    graph

--- graphPage/dashboard.js
+++ graphPage/dashboard.js
@@ -2,38 +2,47 @@
 
 (() => {
   'use strict'
+  // geld in array plaatsen -> visualizeren
+  const wallet = [10000,0,0,0,0,0,0]
 
-  // Graphs
+      // Graphs
   const ctx = document.getElementById('myChart')
   // eslint-disable-next-line no-unused-vars
   const myChart = new Chart(ctx, {
     type: 'line',
     data: {
       labels: [
-        'Sunday',
-        'Monday',
-        'Tuesday',
-        'Wednesday',
-        'Thursday',
-        'Friday',
-        'Saturday'
+        'turn 1',
+        'turn 2',
+        'turn 3',
+        'turn 4',
+        'turn 5',
+        'turn 6',
+        'turn 7'
       ],
       datasets: [{
+
         data: [
-          15339,
-          21345,
-          18483,
-          24003,
-          23489,
-          24092,
-          12034
+
+            wallet[0],
+            // wallet[1],
+            // wallet[2]
+
+        //   15339,
+        //   21345,
+        //   18483,
+        //   24003,
+        //   23489,
+        //   24092,
+        //   12034
         ],
         lineTension: 0,
         backgroundColor: 'transparent',
         borderColor: '#007bff',
         borderWidth: 4,
         pointBackgroundColor: '#007bff'
-      }]
+      }],
+
     },
     options: {
       plugins: {
